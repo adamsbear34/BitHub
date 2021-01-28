4ab06836f4780904c7d826d40d2ed5071fa9700b
Evgeniy <es1230200306@gmail.com>
2021-01-27 19:55:36 -0500

    Changed Category route

--- routes/api/category.routes.js
+++ routes/api/category.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
 const auth = require('../../middleware/auth');
-const config = require('../../config/default.json');
 const Category = require('../../models/Category');
 const Posts = require('../../models/Posts');
 const slugify = require('slugify');
